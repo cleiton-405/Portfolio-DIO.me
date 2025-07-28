77c8e518d25f24dd4668ecd368f84d8add513d49
Cleiton <cleiton4055@hotmail.com>
2025-07-27 21:42:22 -0300

    Ajustes no script

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function changeTheme(){ // Função para mudar o tema do site
     const currentTheme = rootHtml.getAttribute("data-theme")
 
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light"): rootHtml.setAttribute("data-theme", "dark")
-    // Fazendo a pergunta, usando o operador ternário
+    // Fazendo a pergunta usando o operador ternário
 
     toggleTheme.classList.toggle("bi-piggy-bank") // Ao clicar ele muda o tema
     toggleTheme.classList.toggle("bi-piggy-bank") // Ao clicar ele muda o tema
@@ -15,18 +15,19 @@ function changeTheme(){ // Função para mudar o tema do site
 
 toggleTheme.addEventListener('click', changeTheme) // Executando a função com o clique
 
-accordionHeaders.forEach(header => {
-  header.addEventListener("click", () => {
-    const accordionItem = header.parentElement
-    const accordionActive = accordionItem.classList.contains("active")
+accordionHeaders.forEach(header =>{ // Lista de cabeçalhos de seções de acordeão
+    header.addEventListener("click", () =>{ // Evento de clique
+      const accordionItem = header.parentElement // Pega o elemento pai do cabeçalho
+      const accordionActive = accordionItem.classList.contains("active")
+      // Verifica se o item já está ativo
 
-    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
+      accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active") // Fazendo a pergunta usando o operador ternário
   })
 })
 
-menuLinks.forEach(item => {
-  item.addEventListener("click", () => {
-    menuLinks.forEach(i => i.classList.remove("active"))
-    item.classList.add("active")
-  })
+menuLinks.forEach(item =>{ // Itera sobre todos os itens de menu 
+    item.addEventListener("click", () =>{ // Evento de clique
+      menuLinks.forEach(i => i.classList.remove("active")) // Remove a classe "active"
+      item.classList.add("active") // Adiciona a classe "active" ao item que foi clicado
+      })
 })
